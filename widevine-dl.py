43d30b2d9d5004111b0cc6a29d15d522b22cad7b
rsgrt <98905739+rsgrt@users.noreply.github.com>
2022-09-03 23:02:52 +0800

    Update widevine-dl.py

--- widevine-dl.py
+++ widevine-dl.py
@@ -26,7 +26,7 @@ def empty_folder(folder):
 def download_drm_content(mpd_url):
 	divider()
 	print("Processing Video Info..")
-	os.system(f'ytdlp --no-warnings --allow-unplayable-formats --no-check-certificate -F "%s"'%mpd_url)
+	os.system(f'yt-dlp --no-warnings --allow-unplayable-formats --no-check-certificate -F "%s"'%mpd_url)
 	divider()
 	VIDEO_ID = input("ENTER VIDEO_ID (Press Enter for Best): ")
 	if VIDEO_ID == "":
@@ -38,9 +38,9 @@ def download_drm_content(mpd_url):
 	
 	divider()
 	print("Downloading Encrypted Video from CDN..")	
-	os.system(f'ytdlp -o "{TEMPORARY_PATH}/encrypted_video.%(ext)s" --no-warnings --external-downloader aria2c --allow-unplayable-formats --no-check-certificate -f {VIDEO_ID} "{mpd_url}" -o "{TEMPORARY_PATH}/encrypted_video.%(ext)s"')
+	os.system(f'yt-dlp -o "{TEMPORARY_PATH}/encrypted_video.%(ext)s" --no-warnings --external-downloader aria2c --allow-unplayable-formats --no-check-certificate -f {VIDEO_ID} "{mpd_url}" -o "{TEMPORARY_PATH}/encrypted_video.%(ext)s"')
 	print("Downloading Encrypted Audio from CDN..")
-	os.system(f'ytdlp -o "{TEMPORARY_PATH}/encrypted_audio.m4a" --no-warnings --external-downloader aria2c --allow-unplayable-formats --no-check-certificate -f {AUDIO_ID} "{mpd_url}"')
+	os.system(f'yt-dlp -o "{TEMPORARY_PATH}/encrypted_audio.m4a" --no-warnings --external-downloader aria2c --allow-unplayable-formats --no-check-certificate -f {AUDIO_ID} "{mpd_url}"')
 
 def decrypt_content():
 	divider()

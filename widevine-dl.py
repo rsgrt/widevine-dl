a105838d7d7c65e4c20b82dc5f3bdf5f8cc7d555
rsgrt <98905739+rsgrt@users.noreply.github.com>
2022-09-03 22:54:04 +0800

    Update widevine-dl.py

--- widevine-dl.py
+++ widevine-dl.py
@@ -11,13 +11,9 @@ import time
 FILE_DIRECTORY=str(pathlib.Path(__file__).parent.absolute())
 TEMPORARY_PATH = FILE_DIRECTORY+"/cache"
 OUTPUT_PATH = FILE_DIRECTORY+"/output"
-
-def osinfo():
-	global PLATFORM
-	if platform.system()== "Darwin":
-		PLATFORM = "Mac"
-	else:
-		PLATFORM = platform.system()
+ffmpeg = "D:\\path\\ffmpeg.exe"
+ytdlp = "D:\\path\\yt-dlp.exe"
+aria2c = "D:\\path\\aria2c.exe"
 		
 def divider():
 	print ('-' * shutil.get_terminal_size().columns)
@@ -29,18 +25,11 @@ def empty_folder(folder):
 	print("Emptied Temporary Files!")
 	divider()
 	quit()
-	
-def extract_key (prompt):
-	global key,kid,keys
-	key = prompt[30 : 62]
-	kid = prompt[68 : 100]
-	keys = "%s:%s"%(kid,key)
-	return key,kid,keys
 
 def download_drm_content(mpd_url):
 	divider()
 	print("Processing Video Info..")
-	os.system('yt-dlp --external-downloader aria2c --no-warnings --allow-unplayable-formats --no-check-certificate -F "%s"'%mpd_url)
+	os.system(f'{ytdlp} --external-downloader {aria2c} --no-warnings --allow-unplayable-formats --no-check-certificate -F "%s"'%mpd_url)
 	divider()
 	VIDEO_ID = input("ENTER VIDEO_ID (Press Enter for Best): ")
 	if VIDEO_ID == "":
@@ -52,26 +41,15 @@ def download_drm_content(mpd_url):
 	
 	divider()
 	print("Downloading Encrypted Video from CDN..")	
-	os.system(f'yt-dlp -o "{TEMPORARY_PATH}/encrypted_video.%(ext)s" --no-warnings --external-downloader aria2c --allow-unplayable-formats --no-check-certificate -f {VIDEO_ID} "{mpd_url}" -o "{TEMPORARY_PATH}/encrypted_video.%(ext)s"')
+	os.system(f'{ytdlp} -o "{TEMPORARY_PATH}/encrypted_video.%(ext)s" --no-warnings --external-downloader {aria2c} --allow-unplayable-formats --no-check-certificate -f {VIDEO_ID} "{mpd_url}" -o "{TEMPORARY_PATH}/encrypted_video.%(ext)s"')
 	print("Downloading Encrypted Audio from CDN..")
-	os.system(f'yt-dlp -o "{TEMPORARY_PATH}/encrypted_audio.%(ext)s" --no-warnings --external-downloader aria2c --allow-unplayable-formats --no-check-certificate -f {AUDIO_ID} "{mpd_url}"')
-
-VIDEO_ID = "video_avc1"
-AUDIO_ID = "audio_und_mp4a"
+	os.system(f'{ytdlp} -o "{TEMPORARY_PATH}/encrypted_audio.m4a" --no-warnings --external-downloader {aria2c} --allow-unplayable-formats --no-check-certificate -f {AUDIO_ID} "{mpd_url}"')
 
 def decrypt_content():
-	extract_key(KEY_PROMPT)
 	divider()
 	print("Decrypting WideVine DRM.. (Takes some time)")
-	osinfo()
-	if PLATFORM == "Mac":
-		MP4DECRYPT_PATH = "%s/mp4decrypt/mp4decrypt_mac"%FILE_DIRECTORY
-	elif PLATFORM == "Windows":
-		MP4DECRYPT_PATH = "%s/mp4decrypt/mp4decrypt_win.exe"%FILE_DIRECTORY
-	elif PLATFORM == "Linux":
-		MP4DECRYPT_PATH = "%s/mp4decrypt/mp4decrypt_linux"%FILE_DIRECTORY
-	else:
-		MP4DECRYPT_PATH = MP4DECRYPT_PATH = "mp4decrypt"
+
+	MP4DECRYPT_PATH = "%s/mp4decrypt/mp4decrypt_win.exe"%FILE_DIRECTORY
 		
 	os.system('%s %s/encrypted_video.mp4 %s/decrypted_video.mp4 --key %s --show-progress'%(MP4DECRYPT_PATH,TEMPORARY_PATH,TEMPORARY_PATH,keys))
 	os.system('%s %s/encrypted_audio.m4a %s/decrypted_audio.m4a --key %s --show-progress'%(MP4DECRYPT_PATH,TEMPORARY_PATH,TEMPORARY_PATH,keys))
@@ -79,32 +57,26 @@ def decrypt_content():
 
 def merge_content():
 	divider()
-	FILENAME=input("Enter File Name (with extension): \n> ")
-	divider()
 	print("Merging Files and Processing %s.. (Takes a while)"%FILENAME)
 	time.sleep(2)
-	os.system('ffmpeg -i %s/decrypted_video.mp4 -i %s/decrypted_audio.m4a -c:v copy -c:a copy %s/%s'%(TEMPORARY_PATH,TEMPORARY_PATH,OUTPUT_PATH,FILENAME))
+	os.system(f'{ffmpeg} -i %s/decrypted_video.mp4 -i %s/decrypted_audio.m4a -c:v copy -c:a copy %s/%s'%(TEMPORARY_PATH,TEMPORARY_PATH,OUTPUT_PATH,FILENAME))
 
 divider()
-print("**** Widevine-DL by vank0n ****")
+print("Widevine-DL by vank0n * mod by r3n\n")
 divider()
-MPD_URL = input("Enter MPD URL: \n> ")
-KEY_PROMPT = input("Enter WideVineDecryptor Prompt: \n> ")
+line_in = input("Enter your full line here: ")
+line_in_split = line_in.split(';')
+
+ASK_FILENAME = line_in_split[0]
+MPD_URL = line_in_split[1]
+KEY_PROMPT = line_in_split[2]
+FILENAME = f'{ASK_FILENAME}.mp4'
+keys = KEY_PROMPT
+
 download_drm_content(MPD_URL)
 decrypt_content()
 merge_content()
 divider()
 print("Process Finished. Final Video File is saved in /output directory.")
 divider()
-
-delete_choice = input("Delete cache files? (y/n)\ny) Yes (default)\nn) No\ny/n> ")
-
-if delete_choice == "n":
-	divider()
-else:
-	empty_folder(TEMPORARY_PATH)
-
-
-
-		
-	
+empty_folder(TEMPORARY_PATH)
